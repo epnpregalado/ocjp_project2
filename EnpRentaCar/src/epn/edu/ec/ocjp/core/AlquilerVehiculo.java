1dbb901b08d1fd9ecc2293b5aae60605c1c83db3
admin <admin@port_cat06>
2016-08-03 00:28:04 -0500

    Subir archivos

--- EnpRentaCar/src/epn/edu/ec/ocjp/core/AlquilerVehiculo.java
+++ EnpRentaCar/src/epn/edu/ec/ocjp/core/AlquilerVehiculo.java
@@ -14,7 +14,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
